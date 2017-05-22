4f3f9c74b3a9031d8e71acf47b13ab3dc7d46d7b
daitu_liang <lxliang1101@163.com>
2017-05-22 16:53:52 +0800

    Update FileUtil.java

--- app/src/main/java/com/baidu/ocr/user/FileUtil.java
+++ app/src/main/java/com/baidu/ocr/user/FileUtil.java
@@ -6,7 +6,7 @@ package com.baidu.ocr.user;
 import java.io.File;
 
 import android.content.Context;
-
+//文件的操作工具类
 public class FileUtil {
     public static File getSaveFile(Context context) {
         File file = new File(context.getFilesDir(), "pic.jpg");
